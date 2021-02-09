65f3f208c7e0e1128c3d62343cd1e14c79f3c390
Abadaike Collins <abadaikecollins@gmail.com>
2021-02-09 14:24:43 +0100

    bar and pie chat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import BarChart from './Components/BarChart';
+import PieChart from './Components/PieChart';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>React Graphs</h1>
+			<div className={'ui container'}>
+				<BarChart />
+				<br />
+				<PieChart />
+			</div>
+		</div>
+	);
 }
 
 export default App;
